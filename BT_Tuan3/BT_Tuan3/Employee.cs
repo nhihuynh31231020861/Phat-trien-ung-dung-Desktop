9fa7ab7fa15c9a957b44f6c0e99da5ed4f3a16bc
nhihuynh31231020861 <nhihuynh.31231020861@st.ueh.edu.vn>
2025-10-28 22:54:23 +0700

    Add files via upload

---
+++ BT_Tuan3/BT_Tuan3/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace BT_Tuan3
+{
+    internal class Employee
+    {
+        public string Id { get; set; }
+        public string FullName { get; set; }
+        public string Position { get; set; }
+    }
+}
---
+++ BT_Tuan3/BT_Tuan3/Product.cs
@@ -0,0 +1,17 @@
+﻿namespace BT_Tuan3
+{
+    internal class Product
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public string Category { get; set; }
+
+        public Product() { }
+        public Product(string code, string name, decimal price, int stock, string category)
+        {
+            Code = code; Name = name; Price = price; Stock = stock; Category = category;
+        }
+    }
+}
