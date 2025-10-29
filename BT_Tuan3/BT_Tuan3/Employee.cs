361e960dfb545bd97350b91d3439b3a8109e1353
nhihuynh31231020861 <nhihuynh.31231020861@st.ueh.edu.vn>
2025-10-29 22:51:08 +0700

    Delete BT_Tuan3 directory

--- BT_Tuan3/BT_Tuan3/Employee.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BT_Tuan3
-{
-    internal class Employee
-    {
-        public string Id { get; set; }
-        public string FullName { get; set; }
-        public string Position { get; set; }
-    }
-}
